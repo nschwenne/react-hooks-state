a132ee5e24ddcfbb5d833c201cc9290b39468e6a
Nicholas <nschwenne@gmail.com>
2021-08-02 12:35:17 -1000

    commit tree 1077e04740bdde28911b218eb4e1b0c715b8f962

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1000000000);
 
   function increment() {
     setCount(count + 1);
